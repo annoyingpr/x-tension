bedbba462a2c67e410d6b5d5638b842fd7f052c3
Prannoy Lal <prannoylal@gmail.com>
2020-05-03 03:07:32 -0400

    added updateUser and getUserInfo function to index.js

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ const sampleWorkStartTime = new Date().setHours(9,0,0);
 const sampleWorkEndTime = new Date().setHours(17,0,0);
 
 function newUser() {
+    console.log('Creating new user...');
     var userRef = ref.push();
     let userRefKey = userRef.key;
     userRef.set({
@@ -17,12 +18,30 @@ function newUser() {
         workStartTime: sampleWorkStartTime,
         workEndTime: sampleWorkEndTime
     });
+    console.log(`Finished creating new user ${userRefKey}`);
     return userRefKey;
 };
 
 function updateUser(uuid, stepsPerDay = 10000, workStartTime = 9, workEndTime = 17) {
-    
+    console.log(`Started updating user ${uuid}...`);
+    ref.child(uuid).update({
+        stepsPerDay: stepsPerDay,
+        workStartTime: new Date().setHours(workStartTime, 0, 0),
+        workEndTime: new Date().setHours(workEndTime, 0, 0)
+    })
+    console.log(`Finished updating user ${uuid}`);
 };
+
+function getUserInfo(uuid) {
+    console.log(`Getting info for user ${uuid}...`);
+    ref.child(uuid).once('value').then(snap => {
+        console.log(`Finished getting info for user ${uuid}`);
+        return snap.val();
+    })
+}
+
+// updateUser('-M6NvyqrcUI8Atj_39pm', 5000, 10, 20);
+// getUserInfo('-M6NvyqrcUI8Atj_39pm');
 
 // var userRef = ref.push();
 // var userRefKey = userRef.key;
